a7a82b642db26bf46d2dc0d87d10da489288d334
KenyerRamirez <kenyer_2002@hotmail.com>
2024-09-14 18:29:23 -0400

    Feat (Mobile): mobile view complete

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,124 @@
 import React, { useState } from "react";
-import { Box, Avatar, Link } from "@mui/material";
+import {
+  Box,
+  Link,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+} from "@mui/material";
 import "../../css/NavBar.css";
 import { Icon } from "@iconify/react";
 
 const NavBar = ({ children }) => {
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+  const DrawerList = (
+    <Box
+      sx={{ width: 250, backgroundColor: "#17202a", height: "100vh" }}
+      role="presentation"
+    >
+      <Icon
+        onClick={() => setIsMenuOpened(false)}
+        icon="ooui:arrow-previous-ltr"
+        width="28px"
+        height="28px"
+        style={{
+          color: "white",
+          margin: "30px 16px -1px",
+          backgroundColor: "#ffffff1f",
+          borderRadius: 50,
+          padding: 5,
+        }}
+      />
+      <List>
+        <ListItem className="list-item-container" disablePadding>
+          <ListItemButton>
+            <Link
+              className="nav-link-drawer"
+              href="#"
+              onClick={() => setIsMenuOpened(false)}
+            >
+              <Icon
+                icon="material-symbols:id-card"
+                width="24px"
+                height="24px"
+                style={{ color: "white", marginRight: 8 }}
+              />
+              About
+            </Link>
+          </ListItemButton>
+        </ListItem>
+        <ListItem className="list-item-container" disablePadding>
+          <ListItemButton>
+            <Link
+              className="nav-link-drawer"
+              href="#projects"
+              onClick={() => setIsMenuOpened(false)}
+            >
+              <Icon
+                icon="material-symbols:cards"
+                width="24px"
+                height="24px"
+                style={{ color: "white", marginRight: 8 }}
+              />
+              Projects
+            </Link>
+          </ListItemButton>
+        </ListItem>
+        <ListItem className="list-item-container" disablePadding>
+          <ListItemButton>
+            <Link
+              className="nav-link-drawer"
+              href="#skills"
+              onClick={() => setIsMenuOpened(false)}
+            >
+              <Icon
+                icon="icon-park-twotone:muscle"
+                width="24px"
+                height="24px"
+                style={{ color: "white", marginRight: 8 }}
+              />
+              Skills
+            </Link>
+          </ListItemButton>
+        </ListItem>
+        <ListItem className="list-item-container" disablePadding>
+          <ListItemButton>
+            <Link
+              className="nav-link-drawer"
+              href="#experience"
+              onClick={() => setIsMenuOpened(false)}
+            >
+              <Icon
+                icon="ant-design:project-twotone"
+                width="24px"
+                height="24px"
+                style={{ color: "white", marginRight: 8 }}
+              />
+              Experience
+            </Link>
+          </ListItemButton>
+        </ListItem>
+        <ListItem className="list-item-container" disablePadding>
+          <ListItemButton>
+            <Link
+              className="nav-link-drawer"
+              href="#contact"
+              onClick={() => setIsMenuOpened(false)}
+            >
+              <Icon
+                icon="fluent:form-48-filled"
+                width="24px"
+                height="24px"
+                style={{ color: "white", marginRight: 8 }}
+              />
+              Contact me
+            </Link>
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
   return (
     <>
       <Box className="nav-bar">
@@ -33,7 +148,15 @@ const NavBar = ({ children }) => {
             icon="iconamoon:menu-burger-horizontal"
             width={24}
             height={24}
+            onClick={() => setIsMenuOpened(true)}
           />
+          <Drawer
+            anchor="right"
+            open={isMenuOpened}
+            onClose={() => setIsMenuOpened(false)}
+          >
+            {DrawerList}
+          </Drawer>
         </Box>
       </Box>
       <Box className="scroll-container">{children}</Box>
